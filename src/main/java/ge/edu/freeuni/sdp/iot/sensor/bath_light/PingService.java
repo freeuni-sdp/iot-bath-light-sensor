6494d88ba52d53b2c280515f7f8fcc458c6dbc6f
wrinkl3 <al.seward@gmail.com>
2016-06-12 00:57:43 +0400

    modified ping

--- src/main/java/ge/edu/freeuni/sdp/iot/sensor/bath_light/PingService.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/sensor/bath_light/PingService.java
@@ -4,17 +4,10 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
 @Path("/ping")
-@Produces( { MediaType.APPLICATION_JSON})
 public class PingService {
 
   @GET
   public Response get() {
-    return Response.ok().build();
-  }
-
-  @GET
-  @Path("{id}")
-  public Response getJson() {
     return Response.ok().build();
   }
 
