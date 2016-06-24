2c1f7b6980d26180f6c52b135b123139955e2078
babdus <babdusi@gmail.com>
2016-06-24 05:31:57 +0400

    Still trying to fix errors

---
+++ test/ge/edu/freeuni/sdp/iot/sensor/bath_light/PostServiceTest.java
@@ -0,0 +1,28 @@
+package ge.edu.freeuni.sdp.iot.sensor.bath_light;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.*;
+
+public class PostServiceTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(PostService.class);
+    }
+
+    @Test
+    public void post() throws Exception {
+        MyJaxBean jxb = new MyJaxBean();
+        jxb.houseId = "3";
+        jxb.time = "now";
+        jxb.status = "on";
+        Response response = target("status").request().post(Entity.json(jxb));
+    }
+}
