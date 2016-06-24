4429d4b4df9d37526f287875503b6e3ce98f3c6d
babdus <babdusi@gmail.com>
2016-06-24 22:38:01 +0400

    One message to rule them all

--- src/main/java/ge/edu/freeuni/sdp/iot/sensor/bath_light/PostService.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/sensor/bath_light/PostService.java
@@ -8,8 +8,6 @@ public class PostService {
     @POST
     @Consumes("application/json")
     public void post(final MyJaxBean input) {
-        System.out.println("houseId = " + input.houseId);
-        System.out.println("time = " + input.time);
-        System.out.println("status = " + input.status);
+        System.out.println("houseId = " + input.houseId + ", time = " + input.time + ", status = " + input.status);
     }
 }
