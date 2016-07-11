0bc2368c27b709e7b57b1e1a3a7780e563a00e23
wrinkl3 <al.seward@gmail.com>
2016-07-12 01:56:54 +0400

    Added InMemoryRepository for testing purposes

---
+++ src/main/java/ge/edu/freeuni/sdp/iot/sensor/bath_light/model/InMemoryRepository.java
@@ -0,0 +1,46 @@
+package ge.edu.freeuni.sdp.iot.sensor.bath_light.model;
+
+
+import com.microsoft.azure.storage.StorageException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryRepository implements Repository{
+    private static InMemoryRepository instance;
+    protected Map<String, HouseEntity> map;
+
+    public static InMemoryRepository instance() {
+        if (instance==null) {
+            instance = new InMemoryRepository(new HashMap<String, HouseEntity>());
+        }
+        return instance;
+    }
+
+    public InMemoryRepository(Map<String, HouseEntity> map) {
+        super();
+        this.map = map;
+    }
+
+
+    @Override
+    public void insertOrUpdate(HouseEntity bathroom) throws StorageException {
+        String id = bathroom.getRowKey();
+        map.put(id, bathroom);
+    }
+
+    @Override
+    public HouseEntity delete(String id) throws StorageException {
+        return map.remove(id);
+    }
+
+    @Override
+    public HouseEntity find(String id) throws StorageException {
+        return map.get(id);
+    }
+
+    @Override
+    public Iterable<HouseEntity> getAll() {
+        return map.values();
+    }
+}
